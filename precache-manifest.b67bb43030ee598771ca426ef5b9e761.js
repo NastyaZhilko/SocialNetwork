cd9be09b97a48d0fe9d74ab9a3b6803d18546824
NastyaZhilko <nasten4ik2010@gmail.com>
2020-11-29 18:16:02 +0300

    Updates

---
+++ precache-manifest.b67bb43030ee598771ca426ef5b9e761.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c081e9a7dda3ff0cc17e3edb731ef8a5",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "55fe610640d0e0efa60a",
+    "url": "/SocialNetwork/static/css/main.8e04533a.chunk.css"
+  },
+  {
+    "revision": "c6f1a609df1b7653bbcd",
+    "url": "/SocialNetwork/static/js/2.2d88ad4a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/SocialNetwork/static/js/2.2d88ad4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55fe610640d0e0efa60a",
+    "url": "/SocialNetwork/static/js/main.1d434930.chunk.js"
+  },
+  {
+    "revision": "cbcf78d674966714af32",
+    "url": "/SocialNetwork/static/js/runtime-main.bbb63f0e.js"
+  },
+  {
+    "revision": "a1b97f4fec81fea40da90ec2d8f383d0",
+    "url": "/SocialNetwork/static/media/wpapers.a1b97f4f.jpg"
+  }
+]);
