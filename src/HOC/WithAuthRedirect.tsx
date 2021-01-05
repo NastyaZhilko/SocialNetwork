1cd5e419a92d244aabab3f1725351ddd7cb9afdb
NastyaZhilko <nasten4ik2010@gmail.com>
2021-01-06 01:05:34 +0300

    HOC type commit

--- src/HOC/WithAuthRedirect.tsx
+++ src/HOC/WithAuthRedirect.tsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, {ComponentType} from "react";
 import {Redirect} from "react-router-dom";
 import {AppStateType} from "../Redux/redux-store";
 import {connect} from "react-redux";
 
-
+type PropsType={
+    isAuth:boolean
+}
 
 let mapStateToPropsForRedirect = (state: AppStateType) => ({
     isAuth: state.auth.isAuth
-})
+} as PropsType)
 
-export const withAuthRedirect = (Component: React.ComponentType) => {
-    class RedirectComponent extends React.Component<any, {}>{
-        render() {
-            if (!this.props.isAuth) return  <Redirect to='/login'/>
-            return <Component {...this.props}/>
-        }
+type DispatchPropsType = {}
+
+export function withAuthRedirect <WP> (Component: React.ComponentType<WP>)  {
+    function RedirectComponent(props: PropsType & DispatchPropsType){
+        let {isAuth, ...restProps} = props
+            if (!props.isAuth) return  <Redirect to='/login'/>
+            return <Component {...restProps as WP}/>
+
     }
     let ConnectedAuthRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent)
     return ConnectedAuthRedirectComponent
