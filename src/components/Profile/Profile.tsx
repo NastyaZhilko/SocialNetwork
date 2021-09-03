4f9d580a57d846cd778d0e9dc8027914a6e8d7cb
NastyaZhilko <nasten4ik2010@gmail.com>
2021-09-03 15:36:26 +0300

    new apiKey

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 import {ProfileType} from "../../Redux/profile_reducer";
+import s from "./Profile.module.css"
 
 export type ProfilePropsType = {
     isOwner: boolean
@@ -13,7 +14,7 @@ export type ProfilePropsType = {
 const Profile = (props: ProfilePropsType) => {
     console.log('profile')
     return (
-        <div className='app-wrapper-content'>
+        <div >
             {props.profile && <ProfileInfo
                 isOwner={props.isOwner}
                 profile={props.profile}
